c24f7086fe5a66ee3333c1676f248c8d138be87e
chilam <chilam.Lee@outlook.com>
2026-07-31 17:58:31 +0800

    :bug: fix: 修复测试导致读取真实key的问题

--- cmd/aice/main_test.go
+++ cmd/aice/main_test.go
@@ -2,11 +2,53 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 
 	"github.com/ch1lam/aice-cli/internal/config"
 )
+
+func TestMain(m *testing.M) {
+	testHome, err := os.MkdirTemp("", "aice-cmd-test-home-")
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "create isolated test home:", err)
+		os.Exit(1)
+	}
+
+	environment := []struct {
+		key   string
+		value string
+	}{
+		{key: "HOME", value: testHome},
+		{key: "USERPROFILE", value: testHome},
+		{key: config.EnvDeepSeekAPIKey, value: ""},
+		{
+			key:   config.EnvDeepSeekBaseURL,
+			value: "http://127.0.0.1:0",
+		},
+	}
+	for _, variable := range environment {
+		if err := os.Setenv(variable.key, variable.value); err != nil {
+			_, _ = fmt.Fprintf(
+				os.Stderr,
+				"set isolated test environment %s: %v\n",
+				variable.key,
+				err,
+			)
+			_ = os.RemoveAll(testHome)
+			os.Exit(1)
+		}
+	}
+
+	code := m.Run()
+	if err := os.RemoveAll(testHome); err != nil && code == 0 {
+		_, _ = fmt.Fprintln(os.Stderr, "remove isolated test home:", err)
+		code = 1
+	}
+	os.Exit(code)
+}
 
 func TestRunHelp(t *testing.T) {
 	t.Parallel()
